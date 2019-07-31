459a0a497a1e460051bded4f15f72fb01970a5ec
unknown <pampyras@gmail.com>
2019-07-31 15:05:12 +0300

    Update

--- catalog/language/latvian/shipping/omnivalt.php
+++ catalog/language/latvian/shipping/omnivalt.php
@@ -6,5 +6,11 @@ $_['text_parcel_terminal']                     = 'Pakomāts';
 $_['text_select_terminal']                     = 'Izvēlieties paku termināli';
 
 $_['text_omniva_map_head'] = 'Omniva Pakomāts';
-$_['text_omniva_terminal_address'] = 'Pakomāts adrese';
+$_['text_omniva_terminal_address'] = 'Pakomātu adreses';
 $_['text_select_omn_terminal']     = 'Izvēlieties';
+$_['text_search_placeholder']     = 'Ievadiet pasta indeksu/adresi';
+$_['text_not_found']     = 'Vieta nav atrasta';
+$_['text_enter_address']     = 'Ievadiet pasta indeksu';
+$_['text_show_in_map']     = '	Apskatīt novietojumu';
+$_['text_show_more']     = 'Parādīt vairāk';
+$_['text_back_to_list']  = 'Atpakaļ';
